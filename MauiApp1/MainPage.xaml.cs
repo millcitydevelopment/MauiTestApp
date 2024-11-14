2e7a2a618bd0a86a992df5c66f385f78daac9237
Scott Davis <sourcetree@scott-davis.com>
2024-11-14 14:41:34 -0600

    Added symbolic link setup to project file. Still not working with Phidget Nuget

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿//using Phidget22;
+﻿using Phidget22;
 
 namespace MauiApp1;
 
@@ -6,22 +6,22 @@ public partial class MainPage : ContentPage
 {
 	int count = 0;
 
-//	TemperatureSensor temp;
+	TemperatureSensor temp;
 
 	public MainPage()
 	{
 		InitializeComponent();
-//		temp = new TemperatureSensor();
-//		temp.IsLocal = true;
-//		temp.Channel = 0;
-//		temp.Open();
+		temp = new TemperatureSensor();
+		temp.IsLocal = true;
+		temp.Channel = 0;
+		temp.Open();
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
 		string btnText = count + "   Binding Code Temp: " + ServiceLocator.Phidget.GetTemp1();
-//		btnText += "   Phidget NuGet Temp: " +  temp.Temperature;
+		btnText += "   Phidget NuGet Temp: " +  temp.Temperature;
 		CounterBtn.Text = btnText;
 	}
 }
