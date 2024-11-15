d02684569581d3df7b02d60145c10ded9b445649
Scott Davis <sourcetree@scott-davis.com>
2024-11-15 08:53:50 -0600

    Phidget Nuget is working

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -20,7 +20,8 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
-		string btnText = count + "   Binding Code Temp: " + ServiceLocator.Phidget.GetTemp1();
+		string btnText = count.ToString();
+//		btnText += "   Binding Code Temp: " + ServiceLocator.Phidget.GetTemp1();
 		btnText += "   Phidget NuGet Temp: " +  temp.Temperature;
 		CounterBtn.Text = btnText;
 	}
--- MauiApp1/Platforms/MacCatalyst/Program.cs
+++ MauiApp1/Platforms/MacCatalyst/Program.cs
@@ -10,9 +10,9 @@ public class Program
 	// This is the main entry point of the application.
 	static void Main(string[] args)
 	{
-		MacPhidgetManager phdt = new MacPhidgetManager();
-		phdt.Init();
-		ServiceLocator.Phidget = phdt;
+		// MacPhidgetManager phdt = new MacPhidgetManager();
+		// phdt.Init();
+		// ServiceLocator.Phidget = phdt;
 
 		// if you want to use a different Application Delegate class from "AppDelegate"
 		// you can specify it here.
